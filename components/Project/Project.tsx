39df6b52826c50564c78beae94a1036ce61825ff
Aashish Singhal <aashish451singhal@gmail.com>
2021-08-17 17:07:54 +0530

    Project boilerplate added

--- components/Project/Project.tsx
+++ components/Project/Project.tsx
@@ -5,6 +5,17 @@ const Project = () => {
   return (
     <div className={styles.project}>
       <img src="/Assets/images/holder.png" alt="Project Holder" />
+      <br />
+      <div className={styles.metaData}>
+        <div>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit
+            voluptatum vero facere nihil ducimus unde!
+          </p>
+          <div className={styles.tag}></div>
+        </div>
+        <div className={styles.cta}></div>
+      </div>
     </div>
   );
 };
