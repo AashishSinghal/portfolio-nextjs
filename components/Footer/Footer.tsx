ffa069a7b290bb45a27f6f20b8a42ba2f62b812d
Aashish Singhal <aashish451singhal@gmail.com>
2021-08-22 02:24:09 +0530

    Small Bug Fixes and About Page Started..

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,9 +1,11 @@
 import * as React from "react";
+import Socials from "../Socials/Socials";
 import styles from "./Footer.module.scss";
 
 const Footer = () => {
   return (
     <div className={styles.footer}>
+      <Socials />
       All Rights Reserved.
       <br />
       &copy; 2021 Aashish Singhal
