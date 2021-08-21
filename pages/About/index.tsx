614531b9c5cb7e51bbafc2ca6faa12ae2c1d6197
Aashish Singhal <aashish451singhal@gmail.com>
2021-08-22 02:35:25 +0530

    Small Bug Fixes and About Page Started..

--- pages/About/index.tsx
+++ pages/About/index.tsx
@@ -8,6 +8,14 @@ const AboutPage = () => (
       <h1 data-aos="zoom-in" data-aos-delay="300" data-aos-duration="1000">
         More about me...
       </h1>
+      <h2 data-aos="fade-up" data-aos-delay="1300" data-aos-duration="3000">
+        I leverage my Designing Skills and make Web Designs and Bring them to
+        life on the Web with the use of popular frameworks. I intend to succeed
+        in a challenging environment that gives a scope of enhancement to my
+        technical and management skills and make the best use of them in the
+        growth of the organization, I am always eager to learn new things & work
+        on exciting projects.
+      </h2>
     </section>
   </Layout>
 );
