2bff961e41a27632b54724fb6d06ea3306c1a28a
Aashish Singhal <aashish451singhal@gmail.com>
2021-10-31 09:15:04 +0530

    Made some PWA changes, Optimised all Image Assets

--- components/Socials/Socials.tsx
+++ components/Socials/Socials.tsx
@@ -12,9 +12,15 @@ const Socials = () => {
   return (
     <>
       <div className={styles.socials}>
-        <LinkedIn />
-        <GitHub />
-        <Instagram />
+        <a href="https://www.linkedin.com/in/iamaashish5/" target="_blank">
+          <LinkedIn />
+        </a>
+        <a href="https://github.com/AashishSinghal" target="_blank">
+          <GitHub />
+        </a>
+        <a href="https://www.instagram.com/karmabeliever.o_o/" target="_blank">
+          <Instagram />
+        </a>
         <a href="https://wa.me/919672080577" target="_blank">
           <WhatsApp />
         </a>
---
+++ pages/_offline.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+const _offline = () => {
+  return (
+    <div>
+      <h1>❌ Offline ❌</h1>
+      <h2>Please Connect To the Internet.!!</h2>
+    </div>
+  );
+};
+
+export default _offline;
