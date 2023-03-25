1770a608bef93f6633f51e441158a75361868474
Aashish Singhal <aashish451singhal@gmail.com>
2023-03-25 10:25:27 +0000

    saving

---
+++ data/links.ts
@@ -0,0 +1,14 @@
+const links = {
+    instagram: "https://www.instagram.com/i.am.as.is/",
+    linkedin: "https://www.linkedin.com/in/iamaashish5/",
+    twitter: "https://twitter.com/",
+    github: "https://github.com/AashishSinghal",
+    // dribbble: "https://pillai.xyz/dribbble",
+    // dev: "https://pillai.xyz/dev",
+    // facebook: "https://pillai.xyz/facebook",
+    // resume: "https://pillai.xyz/resume-pdf",
+    // repository: "https://github.com/AmruthPillai/ResumeOnTheWeb",
+  };
+  
+  export default links;
+  
